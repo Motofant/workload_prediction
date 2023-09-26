b060e2c2fde5a728b28b29237038a5ac9fe6a482
Motofant <erikzimmermann99@googlemail.com>
2023-09-26 20:17:15 +0200

    adjusted commit

--- data_processing/rolling_window.py
+++ data_processing/rolling_window.py
@@ -26,24 +26,44 @@ for f in files:
 
 print(json.dumps(sorted_files, indent=2))
 
-window_size = pd.Timedelta(seconds=30)
-window_step = pd.Timedelta(seconds=10)
+window_size = pd.Timedelta(seconds=10)
+window_step = pd.Timedelta(seconds=2)
 
 for name in sorted_files.keys():
     #filerreading
-    data_write_key = pd.read_csv([x for x in sorted_files[name]["writing"] if "key_mouse" in x][0], encoding="ISO-8859-1").set_index("time",drop=False)
+    data_write_key = pd.read_csv([x for x in sorted_files[name]["writing"] if "key_mouse" in x][0], encoding="ISO-8859-1",quotechar='"',).set_index("time",drop=False)
+    print(data_write_key)
     data_write_ana = pd.read_csv([x for x in sorted_files[name]["writing"] if "analog" in x][0], 
                                 encoding="ISO-8859-1",delimiter='|', 
                                 dtype={ "perif":"string","location":int,"event":float,},
                                 parse_dates=["time"],
                                 skiprows=[1],
-                                decimal=","
+                                quotechar='"',
+                                decimal=",",
                                 ).set_index("time",drop=False)
     print(len(data_write_ana))
     #data_write_eye = pd.read_csv([x for x in sorted_files[name]["writing"] if "eye" in x][0], encoding="ISO-8859-1").set_index("time",drop=False)
 
-    #data_phrase_key = pd.read_csv([x for x in sorted_files[name]["phrase"] if "key_mouse" in x][0], encoding="ISO-8859-1").set_index("time",drop=False)
-    #data_drag_key = pd.read_csv([x for x in sorted_files[name]["dragging"] if "key_mouse" in x][0], encoding="ISO-8859-1").set_index("time",drop=False)
+    data_phrase_key = pd.read_csv([x for x in sorted_files[name]["phrase"] if "key_mouse" in x][0], encoding="ISO-8859-1",quotechar='"',).set_index("time",drop=False)
+    data_phrase_ana = pd.read_csv([x for x in sorted_files[name]["phrase"] if "analog" in x][0], 
+                                encoding="ISO-8859-1",delimiter='|', 
+                                dtype={ "perif":"string","location":int,"event":float,},
+                                parse_dates=["time"],
+                                skiprows=[1],
+                                quotechar='"',
+                                decimal=",",
+                                ).set_index("time",drop=False)    
+
+    data_phrase_key = pd.read_csv([x for x in sorted_files[name]["phrase"] if "key_mouse" in x][0], encoding="ISO-8859-1",quotechar='"',).set_index("time",drop=False)
+    data_phrase_ana = pd.read_csv([x for x in sorted_files[name]["phrase"] if "analog" in x][0], 
+                                encoding="ISO-8859-1",delimiter='|', 
+                                dtype={ "perif":"string","location":int,"event":float,},
+                                parse_dates=["time"],
+                                skiprows=[1],
+                                quotechar='"',
+                                decimal=",",
+                                ).set_index("time",drop=False)   
+    data_drag_key = pd.read_csv([x for x in sorted_files[name]["dragging"] if "key_mouse" in x][0], encoding="ISO-8859-1",quotechar='"',).set_index("time",drop=False)
     #data_click_key = pd.read_csv([x for x in sorted_files[name]["clicking"] if "key_mouse" in x][0], encoding="ISO-8859-1").set_index("time",drop=False)
 
     # writing
@@ -54,7 +74,7 @@ for name in sorted_files.keys():
     data_write_key["time"] = pd.to_datetime(data_write_key["time"])
     curr = start_val
     print(start_val)
-    output = pd.DataFrame()
+    writing_out = pd.DataFrame()
     # rolling window
     while curr < end_val:
         # get start and end
@@ -68,13 +88,69 @@ for name in sorted_files.keys():
         #key_window.output_string()
         ana_window = Analog(ana)
 
-        out_dict = {"time": curr,**ana_window.output_dict(),}
-        print(out_dict)
-        output = pd.concat([output, pd.DataFrame([out_dict])])
+        write_out_dict = {"time": curr,**ana_window.output_dict(),**key_window.out_dict()}
+        print(write_out_dict)
+        writing_out = pd.concat([writing_out, pd.DataFrame([write_out_dict])])
         curr += window_step
         print(curr)
-    output = output.set_index(pd.DatetimeIndex(output["time"])).drop("time",axis=1)
-    print(output)
+    writing_out = writing_out.set_index(pd.DatetimeIndex(writing_out["time"])).drop("time",axis=1)
+    writing_out.to_csv("./writing.csv")
+    
+    # phrase
+    start_val =pd.to_datetime(data_phrase_key.iloc[0]["time"])
+    end_val =pd.to_datetime(data_phrase_key.iloc[-1]["time"])
+    data_phrase_key = data_phrase_key.set_index("time",drop=False)
+    data_phrase_key.index = pd.to_datetime(data_phrase_key.index)
+    data_phrase_key["time"] = pd.to_datetime(data_phrase_key["time"])
+    curr = start_val
+    print(start_val)
+    phrase_out = pd.DataFrame()
+    # rolling window
+    while curr < end_val:
+        # get start and end
+        diff = curr + window_size
+
+        # get data from every log
+        key = data_phrase_key.loc[(data_phrase_key["time"] >= curr) & (data_phrase_key["time"] < diff)]
+        ana = data_phrase_ana.loc[(data_phrase_ana["time"] >= curr) & (data_phrase_ana["time"] < diff)]
+        #eye = data_write_eye.loc[(data_write_eye["time"] >= curr) & (data_write_eye["time"] < diff)]
+        key_window = KeyMouse(key,window_size)
+        #key_window.output_string()
+        ana_window = Analog(ana)
+
+        phrase_out_dict = {"time": curr,**ana_window.output_dict(),**key_window.out_dict()}
+        print(phrase_out_dict)
+        phrase_out = pd.concat([phrase_out, pd.DataFrame([phrase_out_dict])])
+        curr += window_step
+        print(curr)
+    
+    phrase_out = phrase_out.set_index(pd.DatetimeIndex(phrase_out["time"])).drop("time",axis=1)
+    phrase_out.to_csv("./phrasing.csv")
+    
+    # dragging
+    start_val =pd.to_datetime(data_drag_key.iloc[0]["time"])
+    end_val =pd.to_datetime(data_drag_key.iloc[-1]["time"])
+    data_drag_key = data_drag_key.set_index("time",drop=False)
+    data_drag_key.index = pd.to_datetime(data_drag_key.index)
+    data_drag_key["time"] = pd.to_datetime(data_drag_key["time"])
+    curr = start_val
+    print(start_val)
+    drag_out = pd.DataFrame()
+    # rolling window
+    while curr < end_val:
+        # get start and end
+        diff = curr + window_size
+
+        # get data from every log
+        key = data_drag_key.loc[(data_drag_key["time"] >= curr) & (data_drag_key["time"] < diff)]
+        #eye = data_write_eye.loc[(data_write_eye["time"] >= curr) & (data_write_eye["time"] < diff)]
+        key_window = KeyMouse(key,window_size)
+        drag_out_dict = {"time": curr,**key_window.out_dict()}
+        print(drag_out_dict)
+        drag_out = pd.concat([drag_out, pd.DataFrame([drag_out_dict])])
+        curr += window_step
+    drag_out = drag_out.set_index(pd.DatetimeIndex(drag_out["time"])).drop("time",axis=1)
+    drag_out.to_csv("./dragging.csv")
 exit(1)
 path = "./test.log"
 
